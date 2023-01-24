cc49b1d448e6dfdbd31ad5a0f046973b252de86b
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-24 16:13:09 +0600

    24.01.01

--- server_1/server.js
+++ server_1/server.js
@@ -14,7 +14,7 @@ sequelize
   .then(() => console.log("Database synced"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5050
 app.listen(PORT, () => {
   console.log(`Express app running on port: ${PORT}`);
 });
--- server_2/src/services/stripe.ts
+++
@@ -1,6 +0,0 @@
-import Stripe from 'stripe';
-import { config } from '../config';
-
-export const stripe = new Stripe(config.STRIPE_API_KEY, {
-  apiVersion: '2020-08-27'
-});
