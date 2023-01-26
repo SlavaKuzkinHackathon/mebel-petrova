e944e90f2c3ad2c7ef8e350f6ace4bdb4572271f
SlavaKuzkinHackathon <jess.888@yandex.ru>
2023-01-26 16:59:18 +0600

    26.01-1

--- server_1/server.js
+++ server_1/server.js
@@ -1,6 +1,10 @@
 const { app } = require("./app");
 const { sequelize } = require("./utils/database");
 const { initModels } = require("./utils/initModels");
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+
 
 sequelize
   .authenticate()
@@ -14,7 +18,54 @@ sequelize
   .then(() => console.log("Database synced"))
   .catch((err) => console.log(err));
 
+
+
 const PORT = process.env.PORT || 5050
 app.listen(PORT, () => {
   console.log(`Express app running on port: ${PORT}`);
 });
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Library API",
+      version: '1.0.0',
+    },
+  },
+  apis: ["app.js"],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
+/* 
+const swaggerDefinition = {
+  info: {
+    title: 'psql Registration Swagger API',
+    version: '1.0.0',
+    description: 'Endpoints to test the user registration routes',
+  },
+  host: 'localhost:5050',
+  basePath: '/',
+  securityDefinitions: {
+    bearerAuth: {
+      type: 'apiKey',
+      name: 'Authorization',
+      scheme: 'bearer',
+      in: 'header',
+    },
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); */
